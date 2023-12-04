e4cc62e5da81c10c87a6205c390021de5319415e
Anastasia <nastya.v.m@mail.ru>
2023-12-04 19:55:21 +0700

    Burger menu.

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import s from './Header.module.css'
+import close from './images/close.svg'
+import burger from './images/icons8-меню.svg'
+import cn from 'classnames'
 
 const Header = ({ setActive }) => {
+	const [isOpen, setOpen] = useState()
+
 	return (
 		<div className={s.header}>
 			<div className='container'>
@@ -10,16 +15,31 @@ const Header = ({ setActive }) => {
 						<h3 className={s.title}>Инна Якушенко</h3>
 						<h5 className={s.subtitle}>Перманент Make Up</h5>
 					</div>
-					<div className={s.nav}>
+					<div
+						className={cn(s.nav, {
+							[s.active]: isOpen,
+						})}
+						onClick={() => setOpen(false)}
+					>
 						<div className={s.info}>
 							<p className={s.time}>с 9:00 до 20:00</p>
 							<p className={s.phone}>+7 913 788 07 01</p>
 						</div>
-						<button className={s.btn} onClick={() => setActive(true)}>
+						<button
+							className={cn(s.btn, s.first)}
+							onClick={() => setActive(true)}
+						>
 							Контакты
 						</button>
-						<button className={s.btn}>Записаться</button>
+						<button className={s.btn}>Обо мне</button>
 					</div>
+					<button className={s.menuBtn} onClick={() => setOpen(!isOpen)}>
+						{isOpen ? (
+							<img className={s.burgerIcon} src={close} />
+						) : (
+							<img className={s.burgerIcon} src={burger} />
+						)}
+					</button>
 				</div>
 			</div>
 		</div>
